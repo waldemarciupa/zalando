ddf1c19b7cb4fc4135e7f3e3d907c8a67d894b13
waldemarciupa <waldemarciupa90@gmail.com>
2021-05-24 21:46:17 +0200

    working on main page slider

--- components/MainSlider.js
+++ components/MainSlider.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import styled from 'styled-components'
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,24 +8,32 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/effect-fade/effect-fade.min.css"
 import "swiper/components/navigation/navigation.min.css"
-import "swiper/components/pagination/pagination.min.css"
-
+import "swiper/components/scrollbar/scrollbar.min.css"
 // import Swiper core and required modules
 import SwiperCore, {
-    EffectFade, Navigation, Pagination
+    Autoplay, EffectFade, Navigation, Scrollbar
 } from 'swiper/core';
 
 // install Swiper modules
-SwiperCore.use([EffectFade, Navigation, Pagination]);
+SwiperCore.use([Autoplay, EffectFade, Navigation, Scrollbar]);
 
 export default function MainSlider() {
 
     return (
         <>
-            <Swiper spaceBetween={30} effect={'fade'} navigation={true} pagination={{
-                "clickable": true
-            }} className="mySwiper">
-                <SwiperSlide>
+            <Swiper
+                spaceBetween={30}
+                effect={'fade'}
+                navigation={true}
+                scrollbar={{
+                    "hide": true
+                }}
+                autoplay={{
+                    "delay": 2500,
+                    "disableOnInteraction": false
+                }}
+                className="mySwiper">
+                <SwiperSlide style={{ background: "#0D6DFF" }}>
                     <Image
                         src="/images/slider1.jpg"
                         width={699}
@@ -32,7 +41,7 @@ export default function MainSlider() {
                         alt=""
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide style={{ background: "#FE584D" }}>
                     <Image
                         src="/images/slider2.jpg"
                         width={699}
@@ -40,7 +49,7 @@ export default function MainSlider() {
                         alt=""
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide style={{ background: "#6328E0" }}>
                     <Image
                         src="/images/slider3.jpg"
                         width={699}
@@ -48,7 +57,7 @@ export default function MainSlider() {
                         alt=""
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide style={{ background: "#4C8419" }}>
                     <Image
                         src="/images/slider4.jpg"
                         width={699}
@@ -56,7 +65,7 @@ export default function MainSlider() {
                         alt=""
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide style={{ background: "#0C8368" }}>
                     <Image
                         src="/images/slider5.jpg"
                         width={699}
